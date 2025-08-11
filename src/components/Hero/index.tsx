74e6533c6950d7292d552262b6de7f033da7ad7b
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2025-08-10 18:40:16 -0600

    Update index.tsx
    Update text in hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -35,33 +35,30 @@ const Hero = () => {
                   </div>
                   <LogoText />
                 </div>
-
-
-
-
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Free and Open-Source Next.js Template for Startup & SaaS
+                  Impulsa tu presencia digital con soluciones a tu medida
                 </h1>
-                <p className="mb-12 text-base leading-relaxed! text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
-                  Startup is free Next.js template for startups and SaaS
-                  business websites comes with all the essential pages,
-                  components, and sections you need to launch a complete
-                  business website, built-with Next 13.x and Tailwind CSS.
+                <p className="mb-12 text-base leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                  Diseñamos y desarrollamos sitios web y aplicaciones modernas, rápidas y seguras,
+                  pensadas para que tu negocio destaque y conecte con más clientes.
+                  Desde emprendedores que inician su camino, hasta empresas consolidadas que buscan
+                  optimizar su presencia online: transformamos ideas en experiencias digitales que generan resultados.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    href="/contact"
                     className="rounded-xs bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                    🔥 Get Pro
+                    🚀 Comienza tu proyecto
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="/services"
                     className="inline-block rounded-xs bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                   >
-                    Star on GitHub
+                    Conoce nuestros servicios
                   </Link>
                 </div>
+
               </div>
             </div>
           </div>
