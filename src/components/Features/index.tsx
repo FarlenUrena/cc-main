c6a41c65821a763c9e4a471b5de28f446aef7604
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2025-08-10 19:28:00 -0600

    features as services

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -8,8 +8,8 @@ const Features = () => {
       <section id="features" className="py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
-            title="Main Features"
-            paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+            title="Servicios"
+            paragraph="En Code Craftz diseñamos y desarrollamos soluciones digitales a la medida para impulsar tu negocio. Desde sitios web informativos hasta aplicaciones rápidas y seguras, te ayudamos a conectar mejor con tus clientes, dar credivilidad a tu negocio y crecer online."
             center
           />
 
