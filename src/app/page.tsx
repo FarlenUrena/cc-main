44088ed7f301ab0321854dc98164fb9320bb6ffc
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2025-08-30 02:12:35 -0600

    Refactor, only necessary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Code Craftz",
+  description: "Empresa enfocada en páginas y aplicaciones web",
   // other metadata
 };
 
